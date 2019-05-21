0381a51ff72d7908310874acb49453e751419e71
Sakhile MSIBI <smsibi@c5r8s11.wethinkcode.co.za>
2019-05-21 12:12:16 +0200

    ft_strncat function

---
+++ ft_strncat.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smsibi <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/21 12:03:43 by smsibi            #+#    #+#             */
+/*   Updated: 2019/05/21 12:11:56 by smsibi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+
+char	*ft_strncat(char *restrict dest, const char *restrict src, size_t n)
+{
+	size_t i;
+	size_t j;
+
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+		i++;
+	while (j < n && src[j] != '\0')
+	{
+		dest[i + j] = src[j];
+		j++;
+	}
+	dest[i + j] = '\0';
+	return (dest);
+}
