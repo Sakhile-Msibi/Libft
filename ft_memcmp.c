99414e2e938ab3305e78c882c66ef15e1408b317
Sakhile MSIBI <smsibi@c5r8s11.wethinkcode.co.za>
2019-05-24 07:52:20 +0200

    ft_memcmp function

---
+++ ft_memcmp.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smsibi <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/24 07:28:15 by smsibi            #+#    #+#             */
+/*   Updated: 2019/05/24 07:51:50 by smsibi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+
+int		ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	size_t i;
+	const unsigned char *ptr1;
+	const unsigned char *ptr2;
+
+	ptr1 = (const unsigned char *)s1;
+	ptr2 = (const unsigned char *)s2;
+	i = 0;
+	while (i < n)
+	{
+		if (*ptr1 < *ptr2)
+			return (-1);
+		else if (*ptr1 > *ptr2)
+			return (1);
+		ptr1++;
+		ptr2++;
+		i++;
+	}
+	return (0);
+}
