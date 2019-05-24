d59617738c48555bc4b0843fbeb2d0026565e81d
Sakhile MSIBI <smsibi@c5r8s11.wethinkcode.co.za>
2019-05-24 14:10:09 +0200

    ft_memccpy function

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,21 +6,21 @@
 /*   By: smsibi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/23 11:37:59 by smsibi            #+#    #+#             */
-/*   Updated: 2019/05/23 11:53:04 by smsibi           ###   ########.fr       */
+/*   Updated: 2019/05/24 14:09:17 by smsibi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <string.h>
 
-void	*ft_memccpy(void *restrict dst, const void *restrict src, int c,size_t n)
+void	*ft_memccpy(void *restrict d, const void *restrict s, int c, size_t n)
 {
-	size_t i;
-	char *s1;
-	char *s2;
-	const char *s3;
+	size_t		i;
+	char		*s1;
+	char		*s2;
+	const char	*s3;
 
-	s1 = (char *)dst;
-	s3 = (const char *)src;
+	s1 = (char *)d;
+	s3 = (const char *)s;
 	i = 0;
 	s2 = 0;
 	while (i < n && s2 == 0)
