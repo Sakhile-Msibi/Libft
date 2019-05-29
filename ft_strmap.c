a914078c1570ee4a2c4c3b2cb8d957e3542fb44c
Sakhile MSIBI <smsibi@c5r7s10.wethinkcode.co.za>
2019-05-29 11:26:46 +0200

    ft_strmap function

---
+++ ft_strmap.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smsibi <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/29 09:24:37 by smsibi            #+#    #+#             */
+/*   Updated: 2019/05/29 11:26:12 by smsibi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+#include <stdlib.h>
+
+size_t	ft_strlen(const char *str)
+{
+	size_t i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strmap(char const *s, char (*f)(char))
+{
+	int i;
+	char *fresh;
+	if (!(fresh = (char *)malloc(sizeof(char) * ft_strlen(s) + 1)))
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		fresh[i] = f((char)s[i]);
+		i++;
+	}
+	return (fresh);
+}
