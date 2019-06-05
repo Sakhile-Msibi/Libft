51748ec190ed3e175f06e0714cf86ea50f4ab303
Sakhile MSIBI <smsibi@c5r7s10.wethinkcode.co.za>
2019-06-05 11:39:20 +0200

    ft_memchr function

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: smsibi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/24 07:03:31 by smsibi            #+#    #+#             */
-/*   Updated: 2019/05/24 07:03:44 by smsibi           ###   ########.fr       */
+/*   Updated: 2019/06/05 11:38:48 by smsibi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,18 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	size_t i;
-	unsigned char *s1;
+	size_t			i;
+	unsigned char	*s1;
 
 	s1 = (unsigned char *)s;
 	i = 0;
 	while (i < n)
 	{
 		if (*s1 == c)
-			return s1;
+			return (s1);
 		else
 			s1++;
 		i++;
 	}
-i	return (0);
+	return (0);
 }
