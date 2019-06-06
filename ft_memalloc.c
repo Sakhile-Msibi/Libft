b143bb52ce8968b6e94859ea1418769dcb109cd9
Sakhile MSIBI <smsibi@c5r7s10.wethinkcode.co.za>
2019-06-06 14:31:52 +0200

    ft_memalloc function

--- ft_memalloc.c
+++ ft_memalloc.c
@@ -6,7 +6,7 @@
 /*   By: smsibi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/27 13:33:58 by smsibi            #+#    #+#             */
-/*   Updated: 2019/05/27 13:46:29 by smsibi           ###   ########.fr       */
+/*   Updated: 2019/06/06 14:31:28 by smsibi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,17 @@
 
 void	*ft_memalloc(size_t size)
 {
-	char *s;
+	char	*s;
+	size_t	i;
 
 	s = NULL;
-	if (size)
+	i = 0;
+	if (!(s = (char *)malloc(size)))
+		return (NULL);
+	while (i < size)
 	{
-		if (!(s = (char *)malloc(size)))
-			return (NULL);
-		while (size)
-		{
-			s[size] = 0;
-			size--;
-		}
+		s[i] = 0;
+		i++;
 	}
-	return ((void *)s);
+	return (s);
 }
