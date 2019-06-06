50d06fe159b78bb10d09a7ac6e16fd0416c92db9
Sakhile MSIBI <smsibi@c5r7s10.wethinkcode.co.za>
2019-06-06 15:17:14 +0200

    ft_putnbr_fd function

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: smsibi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/30 12:38:49 by smsibi            #+#    #+#             */
-/*   Updated: 2019/05/30 12:43:43 by smsibi           ###   ########.fr       */
+/*   Updated: 2019/06/06 15:16:48 by smsibi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,23 @@ void	ft_putchar_fd(char c, int fd)
 	write(fd, &c, 1);
 }
 
+void	ft_putstr_fd(char const *s, int fd)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		ft_putchar_fd(s[i], fd);
+		i++;
+	}
+}
+
 void	ft_putnbr_fd(int n, int fd)
 {
-	if (n < 0)
+	if (n == -2147483648)
+		ft_putstr_fd("-2147483648", fd);
+	else if (n < 0)
 	{
 		ft_putchar_fd('-', fd);
 		ft_putnbr_fd(-n, fd);
@@ -27,7 +41,7 @@ void	ft_putnbr_fd(int n, int fd)
 	else if (n > 9)
 	{
 		ft_putnbr_fd(n / 10, fd);
-		ft_putnbr_fd(n % 10, fd);
+		ft_putchar_fd((n % 10) + '0', fd);
 	}
 	else
 		ft_putchar_fd(n + '0', fd);
