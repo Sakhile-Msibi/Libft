6ed14c4d4eb5a48798a3730224dde486ffb48f92
Sakhile MSIBI <smsibi@c5r7s10.wethinkcode.co.za>
2019-06-06 15:01:42 +0200

    ft_putnbr function

--- ft_putnbr.c
+++ ft_putnbr.c
@@ -6,7 +6,7 @@
 /*   By: smsibi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/27 09:58:08 by smsibi            #+#    #+#             */
-/*   Updated: 2019/05/27 10:12:27 by smsibi           ###   ########.fr       */
+/*   Updated: 2019/06/06 15:01:03 by smsibi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,23 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
+void	ft_putstr(char const *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		write(1, &s[i], 1);
+		i++;
+	}
+}
+
 void	ft_putnbr(int n)
 {
-	if (n < 0)
+	if (n == -2147483648)
+		ft_putstr("-2147483648");
+	else if (n < 0)
 	{
 		ft_putchar('-');
 		ft_putnbr(-n);
@@ -27,7 +41,7 @@ void	ft_putnbr(int n)
 	else if (n > 9)
 	{
 		ft_putnbr(n / 10);
-		ft_putnbr(n % 10);
+		ft_putchar((n % 10) + '0');
 	}
 	else
 		ft_putchar(n + '0');
