ab12052b15865c67aa43383d4e9f3f07096308b5
Sakhile MSIBI <smsibi@c5r7s10.wethinkcode.co.za>
2019-06-07 16:15:18 +0200

    libft header file

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: smsibi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/06 07:42:11 by smsibi            #+#    #+#             */
-/*   Updated: 2019/06/06 12:33:01 by smsibi           ###   ########.fr       */
+/*   Updated: 2019/06/07 15:47:52 by smsibi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,6 +43,7 @@ int		ft_atoi(const char *str);
 char	*ft_strstr(const char *haystick, const char *needle);
 int		ft_tolower(int c);
 int		ft_toupper(int c);
+char	*ft_strnstr(const char *s1, const char *s2, size_t n);
 void	ft_putchar(char c);
 void	ft_putstr(char const *s);
 void	ft_putnbr(int n);
@@ -59,8 +60,8 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 int		ft_strequ(char const *s1, char const *s2);
 int		ft_strnequ(char const *s1, char const *s2, size_t n);
 char	*ft_strsub(char const *s, unsigned int start, size_t len);
-char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s);
+char	*ft_strjoin(char const *s1, char const *s2);
 char	**ft_strsplit(char const *s, char c);
 void	ft_putendl(char const *s);
 void	ft_putchar_fd(char c, int fd);
