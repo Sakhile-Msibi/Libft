f3acb84a849a0f25a7b917f535e6a662f1598725
Sakhile MSIBI <smsibi@c5r7s10.wethinkcode.co.za>
2019-06-11 07:50:35 +0200

    ft_strstr function

--- ft_strstr.c
+++ ft_strstr.c
@@ -6,43 +6,11 @@
 /*   By: smsibi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/22 11:01:36 by smsibi            #+#    #+#             */
-/*   Updated: 2019/06/06 12:19:46 by smsibi           ###   ########.fr       */
+/*   Updated: 2019/06/11 07:49:46 by smsibi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string.h>
-
-int		ft_memcmp(const void *s1, const void *s2, size_t n)
-{
-	size_t				i;
-	const unsigned char	*ptr1;
-	const unsigned char	*ptr2;
-
-	ptr1 = (const unsigned char *)s1;
-	ptr2 = (const unsigned char *)s2;
-	i = 0;
-	while (i < n)
-	{
-		if (*ptr1 < *ptr2)
-			return (-1);
-		else if (*ptr1 > *ptr2)
-			return (1);
-		ptr1++;
-		ptr2++;
-		i++;
-	}
-	return (0);
-}
-
-size_t	ft_strlen(const char *str)
-{
-	size_t i;
-
-	i = 0;
-	while (str[i] != '\0')
-		i++;
-	return (i);
-}
+#include "libft.h"
 
 char	*ft_strstr(const char *s1, const char *s2)
 {
