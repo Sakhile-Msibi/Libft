179af49a96ac7b3aa427e278e07be309ea6e16ec
Sakhile MSIBI <smsibi@c5r7s10.wethinkcode.co.za>
2019-06-11 07:52:51 +0200

    ft_putstr_fd function

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -6,16 +6,11 @@
 /*   By: smsibi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/31 09:30:12 by smsibi            #+#    #+#             */
-/*   Updated: 2019/05/31 09:31:28 by smsibi           ###   ########.fr       */
+/*   Updated: 2019/06/11 07:51:47 by smsibi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-
-void	ft_putchar_fd(char c, int fd)
-{
-	write(fd, &c, 1);
-}
+#include "libft.h"
 
 void	ft_putstr_fd(char const *s, int fd)
 {
