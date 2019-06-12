7cc2da593d5b065f815dca491c5fd27971b14b38
Sakhile MSIBI <smsibi@c5r7s10.wethinkcode.co.za>
2019-06-12 08:06:03 +0200

    ft_tolower function

--- ft_tolower.c
+++ ft_tolower.c
@@ -6,17 +6,13 @@
 /*   By: smsibi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/24 10:59:02 by smsibi            #+#    #+#             */
-/*   Updated: 2019/05/24 10:59:33 by smsibi           ###   ########.fr       */
+/*   Updated: 2019/06/12 08:04:49 by smsibi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int		ft_tolower(int c)
 {
-	unsigned char c1;
-
-	c1 = (unsigned char)c;
-	if (c1 >= 'A' && c1 <= 'Z')
-		return (c1 = c1 + 'a' - 'A');
-	else
-		return (c1);
+	if (c >= 'A' && c <= 'Z')
+		return (c + 'a' - 'A');
+	return (c);
 }
