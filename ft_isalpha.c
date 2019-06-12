34b55f216b1fd72762b4464d10d65b5f7aa1acf3
Sakhile MSIBI <smsibi@c5r7s10.wethinkcode.co.za>
2019-06-12 07:47:45 +0200

    ft_isalpha function

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -6,20 +6,11 @@
 /*   By: smsibi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/24 11:20:52 by smsibi            #+#    #+#             */
-/*   Updated: 2019/05/24 11:48:13 by smsibi           ###   ########.fr       */
+/*   Updated: 2019/06/12 07:42:01 by smsibi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int		ft_isalpha(int c)
 {
-	char			*c1;
-	unsigned char	c2;
-
-	c1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" "abcdefghijklmnopqrstuvwxyz";
-	c2 = (unsigned char)c;
-	while (*c1 != '\0' && *c1 != c2)
-		c1++;
-	if (*c1)
-		return (1);
-	return (0);
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
