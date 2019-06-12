584de954f556abf225d961eb76bc1f5a1e728080
Sakhile MSIBI <smsibi@c5r7s10.wethinkcode.co.za>
2019-06-12 08:02:47 +0200

    ft_isalnum function

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -6,20 +6,12 @@
 /*   By: smsibi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/24 13:10:37 by smsibi            #+#    #+#             */
-/*   Updated: 2019/06/06 12:07:00 by smsibi           ###   ########.fr       */
+/*   Updated: 2019/06/12 07:47:06 by smsibi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int		ft_isalnum(int c)
 {
-	char			*c1;
-	unsigned char	c2;
-
-	c1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" "abcdefghijklmnopqrstuvwxyz" "0123456789";
-	c2 = (unsigned char)c;
-	while (*c1 != '\0' && *c1 != c2)
-		c1++;
-	if (*c1)
-		return (1);
-	return (0);
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') ||\
+			(c >= '0' && c <= '9'));
 }
