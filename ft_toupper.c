5fab173b74e46508fff8f33b4dabf2ee4d351aff
Sakhile MSIBI <smsibi@c5r7s10.wethinkcode.co.za>
2019-06-12 08:04:09 +0200

    ft_toupper function

--- ft_toupper.c
+++ ft_toupper.c
@@ -6,17 +6,13 @@
 /*   By: smsibi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/24 10:35:00 by smsibi            #+#    #+#             */
-/*   Updated: 2019/05/24 10:47:25 by smsibi           ###   ########.fr       */
+/*   Updated: 2019/06/12 07:59:57 by smsibi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int		ft_toupper(int c)
 {
-	unsigned char c1;
-
-	c1 = (unsigned char)c;
-	if (c1 >= 'a' && c1 <= 'z')
-		return (c1 = c1 + 'A' - 'a');
-	else
-		return (c1);
+	if (c >= 'a' && c <= 'z')
+		return (c + 'A' - 'a');
+	return (c);
 }
