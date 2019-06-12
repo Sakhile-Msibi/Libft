e5d0ce6262ba00aa1984b04feeafea6c7a445b02
Sakhile MSIBI <smsibi@c5r7s10.wethinkcode.co.za>
2019-06-12 07:32:42 +0200

    ft_strstr function

--- ft_strstr.c
+++ ft_strstr.c
@@ -6,7 +6,7 @@
 /*   By: smsibi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/22 11:01:36 by smsibi            #+#    #+#             */
-/*   Updated: 2019/06/11 07:49:46 by smsibi           ###   ########.fr       */
+/*   Updated: 2019/06/12 07:32:21 by smsibi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ char	*ft_strstr(const char *s1, const char *s2)
 	size_t n;
 
 	n = ft_strlen(s2);
+	if (*s2 == '\0')
+		return ((char *)s1);
 	while (*s1)
 	{
 		if (!ft_memcmp(s1, s2, n))
