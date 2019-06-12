517bf525f248a642d090f72070e3a8b2b5f1d2ad
Sakhile MSIBI <smsibi@c5r7s10.wethinkcode.co.za>
2019-06-12 09:32:42 +0200

    ft_putstr_fd function

--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -6,7 +6,7 @@
 /*   By: smsibi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/31 09:30:12 by smsibi            #+#    #+#             */
-/*   Updated: 2019/06/11 07:51:47 by smsibi           ###   ########.fr       */
+/*   Updated: 2019/06/12 09:31:33 by smsibi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ void	ft_putstr_fd(char const *s, int fd)
 	int i;
 
 	i = 0;
+	if (!s)
+		return ;
 	while (s[i] != '\0')
 	{
 		ft_putchar_fd(s[i], fd);
