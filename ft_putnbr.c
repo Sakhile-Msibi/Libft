150d59778f068149dbc151f9c98a08137678fe8c
Sakhile MSIBI <smsibi@c5r7s10.wethinkcode.co.za>
2019-06-13 11:18:27 +0200

    ft_putnbr function

--- ft_putnbr.c
+++ ft_putnbr.c
@@ -6,28 +6,11 @@
 /*   By: smsibi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/27 09:58:08 by smsibi            #+#    #+#             */
-/*   Updated: 2019/06/06 15:01:03 by smsibi           ###   ########.fr       */
+/*   Updated: 2019/06/11 07:41:12 by smsibi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
-
-void	ft_putstr(char const *s)
-{
-	int i;
-
-	i = 0;
-	while (s[i] != '\0')
-	{
-		write(1, &s[i], 1);
-		i++;
-	}
-}
+#include "libft.h"
 
 void	ft_putnbr(int n)
 {
