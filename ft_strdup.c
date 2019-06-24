fdbc372cb72f0ff342ca67a677d08433810b3131
Sakhile MSIBI <smsibi@c5r7s10.wethinkcode.co.za>
2019-06-24 07:59:46 +0200

    ft_strdup function

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,11 +6,11 @@
 /*   By: smsibi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 08:00:14 by smsibi            #+#    #+#             */
-/*   Updated: 2019/05/24 14:24:54 by smsibi           ###   ########.fr       */
+/*   Updated: 2019/06/24 07:59:12 by smsibi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
+#include "libft.h"
 
 char	*ft_strdup(const char *src)
 {
