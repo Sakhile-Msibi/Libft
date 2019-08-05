a63dfb332747b535b31817061689cde8f6b26050
Sakhile MSIBI <smsibi@c5r7s10.wethinkcode.co.za>
2019-08-05 08:02:45 +0200

    ft_atoi.c

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,34 +6,24 @@
 /*   By: smsibi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 13:11:18 by smsibi            #+#    #+#             */
-/*   Updated: 2019/06/06 15:46:24 by smsibi           ###   ########.fr       */
+/*   Updated: 2019/08/05 08:00:26 by smsibi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int		ft_atoi(const char *str)
+int		ft_atoi(const char *s)
 {
-	int i;
-	int a;
+	int r;
 	int sign;
 
-	i = 0;
-	a = 0;
-	sign = 1;
-	if (str[i] == '\0')
+	r = 0;
+	if (*s == '\0')
 		return (0);
-	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\v' ||\
-			str[i] == '\n' || str[i] == '\r' || str[i] == '\f')
-		i++;
-	if (str[i] == '-' || str[i] == '+')
-		if (str[i++] == '-')
-			sign *= -1;
-	while (str[i] != '\0')
-	{
-		if (str[i] >= '0' && str[i] <= '9')
-			a = (a * 10) + (str[i] - 48);
-		else
-			return (a * sign);
-		i++;
-	}
-	return (a * sign);
+	while (*s == 32 || (*s >= 9 && *s <= 13))
+		s++;
+	sign = (*s == '-') ? -1 : 1;
+	(*s == '-' || *s == '+') ? s++ : s;
+	while (*s != '\0')
+		if (*s >= '0' && *s <= '9')
+			r = (r * 10) + (*s++ - 48);
+	return (r * sign);
 }
