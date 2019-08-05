1d83a5f0c93b10528fc3a4abd97aba44fd240ec2
Sakhile MSIBI <smsibi@c5r7s10.wethinkcode.co.za>
2019-08-05 08:11:42 +0200

    ft_itoa_base.c

---
+++ ft_itoa_base.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa_base.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smsibi <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/08/05 08:02:57 by smsibi            #+#    #+#             */
+/*   Updated: 2019/08/05 08:11:22 by smsibi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+char	*ft_itoa_base(int value, int base)
+{
+	char	*s;
+	long	n;
+	int		sign;
+	int		i;
+
+	n = (value < 0) ? -(long)value : value;
+	sign = (value < 0 && base == 10) ? -1 : 1;
+	i = (sign == -1) ? 2 : 1;
+	while ((n /= base) >= 1)
+		i++;
+	if (!(s = (char *)malloc(sizeof(char) * (i + 1))))
+		return (NULL);
+	s[i] = '\0';
+	n = (value < 0) ? -(long)value : value;
+	while (i-- + sign)
+	{
+		s[i] = (n % base < 10) ? n % base + '0' : n % base + 'A' - 10;
+		n /= base;
+	}
+	(i == 0) ? s[i] = '-' : 0;
+	return (s);
+}
