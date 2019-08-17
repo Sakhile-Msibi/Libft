8b3a4830953f98c9d2fbe01b0cf0325b3a214673
Sakhile MSIBI <smsibi@c5r7s10.wethinkcode.co.za>
2019-08-17 09:10:23 +0200

    ft_lstnew.c

---
+++ ft_lstnew.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smsibi <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/08/17 09:05:20 by smsibi            #+#    #+#             */
+/*   Updated: 2019/08/17 09:09:41 by smsibi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void const *content, size_t content_size)
+{
+	t_list	*newlist;
+
+	if (!(newlist = (t_list *)malloc(sizeof(*newlist))))
+		return (NULL);
+	if (!(content))
+	{
+		newlist->content = NULL;
+		newlist->content_size = 0;
+	}
+	else
+	{
+		if (!(newlist->content = malloc(content_size)))
+		{
+			free(newlist);
+			return (NULL);
+		}
+		ft_memcpy(newlist->content, content, content_size);
+		newlist->content_size = content_size;
+	}
+	newlist->next = NULL;
+	return (newlist);
+}
