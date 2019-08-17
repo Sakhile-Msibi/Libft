795cbb160b0cb16e611e2c634412a19c91ab66bc
Sakhile MSIBI <smsibi@c5r7s10.wethinkcode.co.za>
2019-08-17 09:31:06 +0200

    libft.h

--- libft.h
+++ libft.h
@@ -6,17 +6,31 @@
 /*   By: smsibi <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/06 07:42:11 by smsibi            #+#    #+#             */
-/*   Updated: 2019/08/16 12:25:52 by smsibi           ###   ########.fr       */
+/*   Updated: 2019/08/17 09:30:28 by smsibi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
-# define BUFF_SIZE 42
+# define BUFF_SIZE 100
+# define FD_ELEM ((t_fd*)current->content)
 # include <unistd.h>
 # include <stdlib.h>
 # include <string.h>
 # include <fcntl.h>
+
+typedef struct		s_list
+{
+	void			*content;
+	size_t			content_size;
+	struct s_list	*next;
+}					t_list;
+
+typedef struct		s_fd
+{
+	int				nfd;
+	char			*rest;
+}					t_fd;
 
 void	ft_bzero(void *s, size_t n);
 int		ft_isalpha(int c);
@@ -34,6 +48,7 @@ char	*ft_strcat(char *restrict s1, const char *restrict s2);
 char	*ft_strchr(const char *s, int c);
 int		ft_strcmp(const char *s1, const char *s2);
 char	*ft_strcpy(char *dst, const char *src);
+int		ft_numlen_max(intmax_t nb, int base);
 char	*ft_strdup(const char *s1);
 size_t	ft_strlcat(char *restrict dst, const char *restrict src, size_t dsize);
 size_t	ft_strlen(const char *s);
@@ -43,7 +58,7 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strncpy(char *dst, const char *src, size_t len);
 char	*ft_strrchr(const char *s, int c);
 int		ft_atoi(const char *str);
-long    ft_atoi_l(const char *s);
+long	ft_atoi_l(const char *s);
 char	*ft_strstr(const char *haystick, const char *needle);
 int		ft_tolower(int c);
 int		ft_toupper(int c);
@@ -54,6 +69,7 @@ void	ft_putstr(char const *s);
 void	ft_putnbr(int n);
 char	*ft_itoa(int n);
 char	*ft_itoa_base(int value, int base);
+char	*ft_itoa_base_max(intmax_t value, int base);
 void	*ft_memalloc(size_t size);
 void	ft_memdel(void **ap);
 char	*ft_strnew(size_t size);
@@ -69,12 +85,15 @@ char	*ft_strsub(char const *s, unsigned int start, size_t len);
 char	*ft_strtrim(char const *s);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strjoin_mod(char *s1, char **s2);
+char	*ft_strjoinfree(char *s1, char *s2, int del)
 char	**ft_strsplit(char const *s, char c);
 void	ft_putendl(char const *s);
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char const *s, int fd);
 void	ft_putendl_fd(char const *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
+t_list	*ft_lstnew(void const *content, size_t content_size);
+void	ft_lstadd(t_list **alst, t_list *new)
 int		get_next_line(const int fd, char **line);
 
 #endif
